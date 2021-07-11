006a027b3fbb60b1647b1793582321ec2fd811ba
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-11 21:08:15 +0700

    chore(shared): remove comment

--- shared/configs/env.go
+++ shared/configs/env.go
@@ -11,7 +11,6 @@ func LoadConfigFromEnv() (maps.Mapper, error) {
 	var result = maps.New()
 	for _, env := range os.Environ() {
 		if k, v, ok := ParseOverride(env); ok {
-			// fmt.Printf("%s\n", env)
 			if key, ok := EnvToKey(k); ok {
 				result.Set(key, v)
 			}
@@ -20,78 +19,3 @@ func LoadConfigFromEnv() (maps.Mapper, error) {
 
 	return result, nil
 }
-
-// func EnvString(name string, def string) string {
-// 	var v, ok = os.LookupEnv(name)
-// 	if ok {
-// 		// log.Debug("found env name '%s' = %s", name, utils.MaskString(v, utils.MEDIUM))
-// 		return v
-// 	}
-
-// 	return def
-// }
-
-// func EnvInt(name string, def int64) int64 {
-// 	var v, ok = os.LookupEnv(name)
-// 	if !ok {
-// 		return def
-// 	}
-
-// 	var i, err = strconv.ParseInt(v, 10, 64)
-// 	if err != nil {
-// 		return def
-// 	}
-
-// 	// log.Debug("found env name '%s' = %d", name, i)
-// 	return i
-// }
-
-// func EnvFloat(name string, def float64) float64 {
-// 	var v, ok = os.LookupEnv(name)
-// 	if !ok {
-// 		return def
-// 	}
-
-// 	var i, err = strconv.ParseFloat(v, 64)
-// 	if err != nil {
-// 		return def
-// 	}
-
-// 	// log.Debug("found env name '%s' = %f", name, i)
-// 	return i
-// }
-
-// func EnvBool(name string, def bool) bool {
-// 	var v, ok = os.LookupEnv(name)
-// 	if !ok {
-// 		return def
-// 	}
-
-// 	var i, err = strconv.ParseBool(v)
-// 	if err != nil {
-// 		return def
-// 	}
-
-// 	// log.Debug("found env name '%s' = %t", name, i)
-// 	return i
-// }
-
-// func GetEnv(name string, def interface{}) interface{} {
-// 	switch v := def.(type) {
-// 	case int:
-// 		return EnvInt(name, int64(v))
-// 	case int64:
-// 		return EnvInt(name, v)
-// 	case float32:
-// 		return EnvFloat(name, float64(v))
-// 	case float64:
-// 		return EnvFloat(name, v)
-// 	case bool:
-// 		return EnvBool(name, v)
-// 	case string:
-// 		return EnvString(name, v)
-// 	default:
-// 		// log.Debug("cannot get env value from not support type (" + name + ")")
-// 		return def
-// 	}
-// }
