ab6b49bb645e6eb731003479b3237f04724b014e
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-13 02:10:26 +0700

    chore(shared): add debug log in merger services

--- shared/maps/utils_merger.go
+++ shared/maps/utils_merger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/kamontat/fthelper/shared/datatype"
+	"github.com/kamontat/fthelper/shared/loggers"
 )
 
 var avoid = []string{"$schema", "#comment#"}
@@ -11,12 +12,15 @@ var avoid = []string{"$schema", "#comment#"}
 // Merge will merge 'a' and 'b'. with optional strategy mapper
 // a will be modifiy to be the result
 func Merge(a, b map[string]interface{}, strategy Mapper) map[string]interface{} {
+	var log = loggers.Get("map", "merger")
+
 	// merge data
 	for key, value := range b {
 		var replaced = false
 		if bData, ok := ToMapper(value); ok {
 			if aData, ok := ToMapper(a[key]); ok {
 				if exist, ok := strategy.Z(key); ok && fmt.Sprint(exist) == fmt.Sprint(MERGER_OVERRIDE) {
+					log.Debug("found merger type: override (map)")
 					a[key] = bData
 					replaced = true
 				} else {
@@ -27,6 +31,7 @@ func Merge(a, b map[string]interface{}, strategy Mapper) map[string]interface{} 
 		} else if bData, ok := datatype.ToArray(value); ok {
 			if aData, ok := datatype.ToArray(a[key]); ok {
 				if exist, ok := strategy.Z(key); ok && exist == MERGER_OVERRIDE {
+					log.Debug("found merger type: override (array)")
 					a[key] = bData
 				} else {
 					a[key] = append(aData, bData...)
@@ -37,6 +42,7 @@ func Merge(a, b map[string]interface{}, strategy Mapper) map[string]interface{} 
 		}
 
 		if !replaced {
+			log.Debug("merging key=%s from %v -> %v", key, a[key], value)
 			a[key] = value
 		}
 	}
