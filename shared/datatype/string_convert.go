0831aa74670d4509bd89d52955442575c27173df
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-22 22:44:31 +0700

    perf(shared): datatype convert will support all kind of int

--- shared/datatype/string_convert.go
+++ shared/datatype/string_convert.go
@@ -4,11 +4,18 @@ import (
 	"strconv"
 )
 
-// convert string to different datatype base on input type
+// convert string to different datatype base on input type.
+// currectly support only int, uint, float, bool, and array.
+// for int, uint and float: this function will always return 64 bits.
 func ConvertStringTo(s string, t interface{}) interface{} {
 	switch t.(type) {
-	case int, int64:
+	case int, int8, int16, int32, int64:
 		i, e := strconv.ParseInt(s, 10, 64)
+		if e == nil {
+			return i
+		}
+	case uint, uint8, uint16, uint32, uint64:
+		i, e := strconv.ParseUint(s, 10, 64)
 		if e == nil {
 			return i
 		}
@@ -30,7 +37,9 @@ func ConvertStringTo(s string, t interface{}) interface{} {
 	return s
 }
 
-// convert string to different datatype
+// convert string to different datatype.
+// currectly support only int64, float64, and boolean
+// if cannot pass it will return input string
 func ConvertString(s string) interface{} {
 	i, e := strconv.ParseInt(s, 10, 64)
 	if e == nil {
