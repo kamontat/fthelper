0c4620150a73718abbb4759a24bb05847fdd4a5a
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-22 22:45:10 +0700

    perf(shared): datatype.ToInt will support all int bits

--- shared/datatype/int.go
+++ shared/datatype/int.go
@@ -3,6 +3,21 @@ package datatype
 import "strconv"
 
 func ToInt(i interface{}) (int64, bool) {
+	in, ok := i.(int)
+	if ok {
+		return int64(in), ok
+	}
+
+	i8, ok := i.(int8)
+	if ok {
+		return int64(i8), ok
+	}
+
+	i16, ok := i.(int16)
+	if ok {
+		return int64(i16), ok
+	}
+
 	i32, ok := i.(int32)
 	if ok {
 		return int64(i32), ok
