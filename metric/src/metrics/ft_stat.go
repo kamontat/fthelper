cffc10e217900bbe9e729c9dbbf578835019a1e2
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-31 21:06:28 +0700

    feat(metric): add new `freqtrade_stat_sell_reason` for win/draw/loss number of each sell reason

--- metric/src/metrics/ft_stat.go
+++ metric/src/metrics/ft_stat.go
@@ -10,6 +10,36 @@ import (
 
 var FT = collectors.NewMetrics(
 	collectors.NewMetric(prometheus.NewDesc(
+		prometheus.BuildFQName("freqtrade", "stat", "sell_reason"),
+		"Sell reason wins/draws/losses number.",
+		append(freqtrade.SummaryLabel(), "reason", "type"),
+		nil,
+	), func(desc *prometheus.Desc, conn connection.Http, param *commands.ExecutorParameter) []prometheus.Metric {
+		var connection = freqtrade.ToConnection(conn)
+		var stats = freqtrade.NewStat(connection)
+
+		var metrics = make([]prometheus.Metric, 0)
+		for name, stat := range stats.Reasons {
+			metrics = append(metrics, prometheus.MustNewConstMetric(
+				desc,
+				prometheus.GaugeValue,
+				float64(stat.Win),
+				append(freqtrade.NewSummary(connection, param.Cache), name, "win")...,
+			), prometheus.MustNewConstMetric(
+				desc,
+				prometheus.GaugeValue,
+				float64(stat.Draw),
+				append(freqtrade.NewSummary(connection, param.Cache), name, "draw")...,
+			), prometheus.MustNewConstMetric(
+				desc,
+				prometheus.GaugeValue,
+				float64(stat.Loss),
+				append(freqtrade.NewSummary(connection, param.Cache), name, "loss")...,
+			))
+		}
+		return metrics
+	}),
+	collectors.NewMetric(prometheus.NewDesc(
 		prometheus.BuildFQName("freqtrade", "stat", "win_duration"),
 		"Current average wins duration",
 		freqtrade.SummaryLabel(),
