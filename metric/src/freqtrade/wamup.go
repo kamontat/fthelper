bab73cc6efa67c9ef27d604643ade6f31fd12d1f
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-07-31 12:53:26 +0700

    perf(metric): warmup process will collect error and report

--- metric/src/freqtrade/wamup.go
+++ metric/src/freqtrade/wamup.go
@@ -3,16 +3,17 @@ package freqtrade
 import (
 	"time"
 
+	"github.com/kamontat/fthelper/shared/errors"
 	"github.com/kamontat/fthelper/shared/loggers"
 )
 
-func Warmup(conn *Connection) (time.Duration, error) {
+func Warmup(conn *Connection) (time.Duration, *errors.Handler) {
 	var logger = loggers.Get("freqtrade", "warmup")
 
-	var err error = nil
+	var err = errors.New()
 	var start = time.Now()
 
-	NewBalance(conn)
+	err.AndD(FetchBalance(conn))
 	NewCount(conn)
 	NewLocks(conn)
 	NewLogs(conn)
@@ -23,8 +24,7 @@ func Warmup(conn *Connection) (time.Duration, error) {
 	NewWhitelist(conn)
 	NewStat(conn)
 
-	err = warmupDailyPerformance(start, conn, logger)
+	err.And(warmupDailyPerformance(start, conn, logger))
 
-	// TODO: Add error handler properly
 	return time.Since(start), err
 }
