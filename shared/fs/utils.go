249d0bd0cd2db2f9cf846c2e561fe37aede2298c
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-08-01 13:48:52 +0700

    chore(shared): move resolveFile from dotenv to ToFiles() in fs module

--- shared/fs/utils.go
+++ shared/fs/utils.go
@@ -15,3 +15,36 @@ func Copy(a, b FileSystem) error {
 
 	return fmt.Errorf("cannot copy from directory (%s) to file (%s)", a.Abs(), b.Abs())
 }
+
+// ToFiles will resolve all directory into file
+// The result of this function can be guarantee type file
+func ToFiles(input []FileSystem) ([]FileSystem, error) {
+	return toFiles(make([]FileSystem, 0), input)
+}
+
+func toFiles(base, files []FileSystem) ([]FileSystem, error) {
+	for _, file := range files {
+		stat, err := file.Stat()
+		if err != nil {
+			return base, err
+		}
+
+		if (*stat).IsDir() {
+			directory, err := NewDirectory(file.Paths())
+			if err != nil {
+				return base, err
+			}
+
+			files, err := directory.ReadDir()
+			if err != nil {
+				return base, err
+			}
+
+			return toFiles(base, files)
+		} else {
+			base = append(base, file)
+		}
+	}
+
+	return base, nil
+}
