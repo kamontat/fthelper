8d37fd5923582dbae6c1e5bee7b288939a306971
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-08-14 23:01:08 +0700

    chore(shared): add more test on runners

--- shared/runners/runner_test.go
+++ shared/runners/runner_test.go
@@ -1,33 +1,76 @@
 package runners_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/kamontat/fthelper/shared/runners"
+	"github.com/kamontat/fthelper/shared/xtests"
 )
 
-func TestCreateRunner(t *testing.T) {
-	t.Run("create simple runner", func(t *testing.T) {
-		runner := runners.NewRunner("test", runners.NoValidate, func(i *runners.SingleInfo) error {
-			return nil
-		})
+func TestSimpleRunner(t *testing.T) {
+	var assertion = xtests.New(t)
 
-		if err := runner.Validate(); err != nil {
-			t.Errorf("validate should return nil error")
-		}
-
-		if err := runner.Run(); err != nil {
-			t.Errorf("run should return nil error")
-		}
-
-		if runner.Information().Status() != runners.SUCCESS {
-			t.Errorf("information must updated to success status")
-		}
-		if runner.Information().Duration() <= 0 {
-			t.Errorf("information must update duration")
-		}
-		if runner.Information().Error().HasError() {
-			t.Errorf("information must not update error")
-		}
+	runner := runners.NewRunner("test", runners.NoValidate, func(i *runners.SingleInfo) error {
+		return nil
 	})
+
+	assertion.NewName("default status is initial").
+		WithActual(runner.Information().Status()).
+		WithExpected(runners.INITIAL).
+		MustEqual()
+
+	assertion.NewName("should be valid").
+		WithError(runner.Validate()).
+		MustNotError()
+
+	assertion.NewName("should be runnable").
+		WithError(runner.Run()).
+		MustNotError()
+
+	assertion.NewName("after run, status is success").
+		WithActual(runner.Information().Status()).
+		WithExpected(runners.SUCCESS).
+		MustEqual()
+
+	assertion.NewName("after run, duration is increase").
+		WithActual(runner.Information().Duration()).
+		WithExpected(0).
+		MustGreaterThan()
+
+	assertion.NewName("after run, no error").
+		WithError(runner.Information().Error().Error()).
+		MustNotError()
+}
+
+func requireInput(i *runners.SingleInfo) error {
+	if i.Input() == nil {
+		return errors.New("input is required")
+	}
+	return nil
+}
+
+func TestInputRunner(t *testing.T) {
+	var assertion = xtests.New(t)
+	runnerWithInput := runners.NewRunner("input", requireInput, runners.NoValidate).Input("hello world")
+	runnerWithoutInput := runners.NewRunner("no input", requireInput, runners.NoValidate)
+
+	assertion.NewName("correct initial input value").
+		WithError(runnerWithInput.Run()).
+		MustNotError()
+
+	assertion.NewName("input is required error").
+		WithError(runnerWithoutInput.Validate()).
+		WithExpected("input is required").
+		MustContainError()
+
+	assertion.NewName("validate didn't rerun").
+		WithError(runnerWithoutInput.Validate()).
+		WithExpected("input is required").
+		MustContainError()
+
+	assertion.NewName("run return error same as validate").
+		WithError(runnerWithoutInput.Run()).
+		WithExpected("input is required").
+		MustContainError()
 }
--- shared/runners/summary_test.go
+++ shared/runners/summary_test.go
@@ -1,11 +1,13 @@
 package runners_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/kamontat/fthelper/shared/loggers"
 	"github.com/kamontat/fthelper/shared/runners"
 	"github.com/kamontat/fthelper/shared/utils"
+	"github.com/kamontat/fthelper/shared/xtests"
 )
 
 func mockRunner() runners.Runner {
@@ -14,11 +16,36 @@ func mockRunner() runners.Runner {
 	})
 }
 
+func mockLogger() (*loggers.Logger, *bytes.Buffer) {
+	var buffer = new(bytes.Buffer)
+	var logger = loggers.Get("test")
+
+	logger.SetWriter(buffer)
+	return logger, buffer
+}
+
 func TestCreateSummary(t *testing.T) {
+	var assertion = xtests.New(t)
 	t.Run("create simple summary from collection", func(t *testing.T) {
+		var logger, writer = mockLogger()
 		c := runners.NewCollection("test", mockRunner(), mockRunner(), mockRunner())
 		summary := runners.ColSummary(c)
+		summary.Log(logger)
 
-		summary.Log(loggers.Get("test"))
+		assertion.New().
+			WithActual(writer.String()).
+			WithExpected("SUMMARY 3 TASKS: success").
+			MustContain()
+	})
+
+	t.Run("create summary from runner", func(t *testing.T) {
+		var logger, writer = mockLogger()
+		summary := runners.RunSummary(mockRunner())
+		summary.Log(logger)
+
+		assertion.New().
+			WithActual(writer.String()).
+			WithExpected("success ( 01/01 - 100.00%)").
+			MustContain()
 	})
 }
