73b5b3f72be8ec5eeaa81a273f5393d67cd24199
Natcha Triratchatchawan <36453933+prangnt@users.noreply.github.com>
2021-10-31 16:16:34 +0700

    chore(doc): add document for error module (#52)
    * add file
    
    * add test case for queue
    
    * add test case
    
    * documentation for schedulers
    
    * revise as comment
    
    * doucmentation for datatype
    
    * Documentation for errors
    
    Co-authored-by: Natcha Triratchatchawan <natcha@Natchas-MacBook-Pro.local>
    Co-authored-by: Kamontat Chantrachirathumrong <developer@kamontat.net>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- shared/errors/handler.go
+++ shared/errors/handler.go
@@ -7,20 +7,24 @@ import (
 	"strings"
 )
 
+// Handler use for checking error
 type Handler struct {
 	// number of total error that check on this handlers. including error that is nil
 	total  int
 	errors []error
 }
 
+// Total will get number of errors
 func (h *Handler) Total() int {
 	return h.total
 }
 
+// Length will get length of errors
 func (h *Handler) Length() int {
 	return len(h.errors)
 }
 
+// And will add error to handler
 func (h *Handler) And(errs ...error) *Handler {
 	for _, err := range errs {
 		h.total += 1
@@ -40,16 +44,19 @@ func (h *Handler) AndD(data interface{}, err error) (interface{}, *Handler) {
 	return data, h.And(err)
 }
 
+// Merge will merge error to handler
 func (h *Handler) Merge(nh *Handler) *Handler {
 	h.errors = append(h.errors, nh.errors...)
 	h.total += nh.total
 	return h
 }
 
+// HasError will check whether handler has error or not
 func (h *Handler) HasError() bool {
 	return len(h.errors) > 0
 }
 
+// First will get the first error in handler
 func (h *Handler) First() error {
 	if h.HasError() {
 		return h.errors[0]
@@ -57,6 +64,7 @@ func (h *Handler) First() error {
 	return nil
 }
 
+// String use for converting object data to string
 func (h *Handler) String() string {
 	var str strings.Builder
 
@@ -72,6 +80,7 @@ func (h *Handler) String() string {
 	return str.String()
 }
 
+// Error will convert handler to error if handler contains any error
 func (h *Handler) Error() error {
 	if h.HasError() {
 		return errors.New(h.String())
@@ -79,16 +88,19 @@ func (h *Handler) Error() error {
 	return nil
 }
 
+// Exit will exit the program if handler contain any error
 func (h *Handler) Exit(n int) {
 	if h.HasError() {
 		os.Exit(n)
 	}
 }
 
+// Same as Exit but always return exit code 1
 func (h *Handler) Exit1() {
 	h.Exit(1)
 }
 
+// Panic program if error
 func (h *Handler) Panic() {
 	if h.HasError() {
 		panic(h.Error())
